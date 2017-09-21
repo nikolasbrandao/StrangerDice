68248547a6e7b7bab56143a9737c2f79a5163bfd
Nikolas Brandão <nikolasbrandao@gmail.com>
2017-09-21 12:36:23 -0300

    Adição de um json de configuração

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const TelegramBot = require(`node-telegram-bot-api`)
+const dadosBot = require('./bot-info.json')
+const bot = new TelegramBot(dadosBot.Token, { polling:true })
 
-const TOKEN = `SEU TOKEN`
-
-const bot = new TelegramBot(TOKEN, { polling:true })
 
 bot.on('message',(msg)=>{
-    console.log('msg', msg);
+    //console.log('msg:', msg)
 })
+
+var logErrorEcho = function logErrorEcho(msg){
+    return function (err){
+        return console.log(msg,err);
+    };
+};
+
+var logSucessEcho = ()=>{
+    return function(data){
+        console.log('Sucess:', data);
+    }
+};
+
+var sendEcho = (msg, match)=>{
+    bot.sendMessage( msg.chat.id, match[ 1 ] )
+        .then( logSucessEcho (msg, match) )
+        .catch( logErrorEcho( 'Error:' ) );
+};
+
+bot.onText( /\/echo (.*)/, sendEcho );
