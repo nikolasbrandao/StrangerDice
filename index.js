d1d4ec22941f17df8f1909236b878831814e4bc3
Nikolas Brandão <nikolasbrandao@gmail.com>
2017-09-25 12:41:32 -0300

    add regra principal de rolagem

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require(`node-telegram-bot-api`)
-//const dadosBot = require('./bot-info.json')
+const dadosBot = require('./bot-info.json')
 
-const bot = new TelegramBot(process.env.api_key, { polling:true })
+const bot = new TelegramBot(dadosBot.Token, { polling:true })
 
 
 bot.on('message',(msg)=>{
@@ -14,16 +14,36 @@ var logErrorEcho = function logErrorEcho(msg){
     };
 };
 
-var logSucessEcho = ()=>{
-    return function(data){
-        console.log('Sucess:', data);
-    }
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 var sendEcho = (msg, match)=>{
     bot.sendMessage( msg.chat.id, match[ 1 ] )
-        .then( logSucessEcho (msg, match) )
+        .then()
+        .catch( logErrorEcho( 'Error:' ) );
+};
+
+var sendDices = (msg, match)=>{
+    //position: 0 = caracter '/', 1 = quantidade de dados, 2 = caracter 'D', 3 = numero de faces no dado  
+    let comando = match[ 0 ].split(/(\d{1,2})/); 
+    let resultado = "Resultados:";
+    for(let i = 1; i <= comando[1]; i++){
+        let rolagem = getRandomInt(1, comando[3]);
+        resultado += "\n Dado "+i+" resultado: "+rolagem;
+    }
+
+    bot.sendMessage( msg.chat.id, resultado )
+        .then( console.log( comando ) )
         .catch( logErrorEcho( 'Error:' ) );
 };
 
 bot.onText( /\/echo (.*)/, sendEcho );
+
+// Expressão regular, comando deve começar com "/"
+// seguido por um numero de no maximo duas casas decimais
+// um caracter "d" minusculo ou maiusculo, e finalizando
+// com um numero de duas casas decimais.
+bot.onText( /\/\b\d{1,2}[dD]\d{1,2}/, sendDices );
